2a633bfb6b5b61aae020ebc1ca0c4e800c30be42
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-14 17:10:18 -0600

    modal added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,28 @@
-// import { useState } from 'react'
-// import React from 'react'
-// import ReactDom from 'react-dom/client';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-const colorchange = {
-  backgroundColor: 'black',
-  color: 'white',
-  height: '100vh',
-  margin: 0
+import { useState } from "react";
+import "./App.css";
+import Modal from "./components/modal";
 
-};
-  
  
-  // const [count, setCount] = useState(0)
+
+const App = () => {
+  const [open, setOpen] = useState(true)
+
+  const colorchange = {
+    backgroundColor: "black",
+    color: "white",
+    height: "100vh",
+    margin: 0,
+  };
+
+
 
   return (
     <>
-    <div style={colorchange}>
-    
-    </div>
-   
-  
-  
-  
+      <div style={colorchange}></div>
 
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+    <Modal open={open} setOpen={setOpen}/>
 
-    </>
-  )
-}
-
-export default App
+     </>
+)}
+export default App;
