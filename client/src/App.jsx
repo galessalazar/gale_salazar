3dfbd56830817da6de09ae1351b562f1b4d3ed71
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-14 16:36:22 -0600

    tailwind install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import React from 'react'
+// import ReactDom from 'react-dom/client';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const colorchange = {
+  backgroundColor: 'black',
+  color: 'white',
+  height: '100vh',
+  margin: 0
+
+};
+  
+ 
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <div style={colorchange}>
+    
+    </div>
+   
+  
+  
+  
+
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +46,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
     </>
   )
 }
