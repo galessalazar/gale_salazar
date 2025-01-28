fdd8a3f33539f27c2d73e678de500691b92fbb8f
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-28 16:16:17 -0600

    added first project to project page

--- client/src/components/ProjectsPage.jsx
+++ client/src/components/ProjectsPage.jsx
@@ -1,5 +1,14 @@
 
 const ProjectsPage = () => {
+  const projects = [
+    {
+      name: 'Lashed up Beauty',
+      description: 'Built for small business owner to showcase different services, pricing, scheduling and a dashboard for technicians.'
+      technologies: ['HTML', 'CSS', 'JAVASCRIPT', 'MERN'],
+      link: 'https://lashed-up-beauty.onrender.com',
+    },
+    
+  ];
   return (
     <div>ProjectsPage</div>
   )
