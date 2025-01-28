a4711dd6b0c08be9afcbc76501a7d36ce595b0c6
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-28 16:53:33 -0600

    added project to projects page

--- client/src/components/ProjectsPage.jsx
+++ client/src/components/ProjectsPage.jsx
@@ -1,17 +1,28 @@
-
 const ProjectsPage = () => {
   const projects = [
     {
-      name: 'Lashed up Beauty',
-      description: 'Built for small business owner to showcase different services, pricing, scheduling and a dashboard for technicians.'
-      technologies: ['HTML', 'CSS', 'JAVASCRIPT', 'MERN'],
-      link: 'https://lashed-up-beauty.onrender.com',
+      name: "Lashed up Beauty",
+      description:
+        "Built for small business owner to showcase different services, pricing, scheduling and a dashboard for technicians.",
+      technologies: ["HTML", "CSS", "JAVASCRIPT", "MERN"],
+      link: "https://lashed-up-beauty.onrender.com",
     },
-    
   ];
   return (
-    <div>ProjectsPage</div>
-  )
-}
+    <div>
+      <h1>ProjectsPage</h1>
+      <div>
+        <h2>{projects[0].name}</h2>
+        {projects[0].image && <img src={projects[0].image} alt={projects[0].name} />}
+          <p>{projects[0].description}</p>
+          <p><strong>Technologies:</strong>{projects[0].technologies.join(', ')}</p>
+          {/* by default,links open in the same tab, the _blank keeps site open while opening link in a new tab */}
+          {/* the noopener and noreferrer are layered security and privacy */}
+          <a href={projects[0].link} target="_blank" rel="noopener noreferrer">View Project</a>
+        
+      </div>
+    </div>
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
