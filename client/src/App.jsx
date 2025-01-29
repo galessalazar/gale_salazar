8840ecb1e368a713b6474841633ba8272d90e3a3
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-28 21:46:11 -0600

    removed the project break

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Modal from "./components/modal";
 import TypeWriterWithSound from "./components/TypeWriterWithSound";
 import ProjectsPage from "./components/ProjectsPage";
@@ -8,13 +13,15 @@ import ProjectsPage from "./components/ProjectsPage";
 const App = () => {
   const [open, setOpen] = useState(true);
   const [text, setText] = useState("");
+  const [typingComplete, setTypingComplete] = useState(false);
   const fullText = "Projects";
 
   const startTyping = () => {
-    // const sound = new Audio("/mechanical_keyboard.ogg");
-    const sound = new Audio('/galessalazar/gale_salazar/mechanical_keyboard.ogg');
+    const sound = new Audio(
+      "/galessalazar/gale_salazar/mechanical_keyboard.ogg"
+    );
     sound.onerror = () => {
-      console.error('failed audio');
+      console.error("failed audio");
     };
     let index = 0;
 
@@ -25,6 +32,8 @@ const App = () => {
         sound.play();
         index++;
         setTimeout(typeLetter, 400);
+      } else{
+        setTypingComplete(true);
       }
     };
 
@@ -33,25 +42,29 @@ const App = () => {
 
   return (
     <Router basename="/galessalazar/gale_salazar">
-      
-
       <Background>
         <Modal open={open} setOpen={setOpen} onContinue={startTyping} />
         <TypeWriterWithSound text={text} />
 
-        <Link to='/projects'>
-          <h2 style={{ cursor: 'pointer', color: 'white', textAlign: 'center'}}>
-            {text}
-
-          </h2>
-        </Link>
+        {/* {typingComplete && (
+            <h2
+              style={{ cursor: "pointer", color: "black", textAlign: "center" }}
+              onClick={() => (window.location.href = '/galessalazar/gale_salazar/projects')} >
+              {text}
+            </h2>
+        )
+        } */}
 
         <Routes>
-          <Route path="/" element={<Navigate to='/projects'/>} />
-          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/" element={<Navigate to="/projects" />} />
+          <Route path="/projects" element={<Background>
+            <ProjectsPage />
+            </Background>
+            }
+             />
         </Routes>
       </Background>
-      </Router>
+    </Router>
   );
 };
 
--- client/src/components/ProjectsPage.jsx
+++ client/src/components/ProjectsPage.jsx
@@ -15,7 +15,7 @@ const ProjectsPage = () => {
         <h2>{projects[0].name}</h2>
         {projects[0].image && <img src={projects[0].image} alt={projects[0].name} />}
           <p>{projects[0].description}</p>
-          <p><strong>Technologies:</strong>{projects[0].technologies.join(', ')}</p>
+          <p><strong>Technologies: </strong>{projects[0].technologies.join(', ')}</p>
           {/* by default,links open in the same tab, the _blank keeps site open while opening link in a new tab */}
           {/* the noopener and noreferrer are layered security and privacy */}
           <a href={projects[0].link} target="_blank" rel="noopener noreferrer">View Project</a>
