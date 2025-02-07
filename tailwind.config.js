e06fda07659faa989c859bf5b3ede27e9c516e39
Al Bundy <gale.s.salazar22@gmail.com>
2025-02-06 21:36:11 -0600

    Updates

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./client/index.html','./client/src/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
