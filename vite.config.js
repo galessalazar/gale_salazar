dae5b3db85db523c04ddd26308d0f88e6a704bb4
Al Bundy <gale.s.salazar22@gmail.com>
2025-02-06 21:18:42 -0600

    moved index to root

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   // name of repo
   base: '/gale_salazar/',
+  build: {
+    outDir: 'dist',
+  }
 })
