a146df68710cd467f9ef73f84b6fa6fde6b9d265
Al Bundy <gale.s.salazar22@gmail.com>
2025-02-11 15:47:48 -0600

    adjusted package json paths

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -62,7 +62,7 @@ export default function Modal({ onContinue }) {
               <button
                 type="button"
                 onClick={handleContinue}
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 sm:ml-3 sm:w-auto"
               >
                 Continue to portfolio...
               </button>
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: './',
   // name of repo
   base: '/gale_salazar/',
   build: {
-    outDir: 'dist',
+    outDir: 'client/dist',
   }
 })
