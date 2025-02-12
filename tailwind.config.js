2fc18ee73f99f2f5eea412e15e16e702ac9580e0
galessalazar <gale.s.salazar22@gmail.com>
2025-02-11 23:39:39 -0600

    update tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./client/index.html','./client/src/**/*.{js,jsx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
