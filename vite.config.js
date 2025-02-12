e6064dc1ccb33443a8f6d9fb9d19632c9925f8e3
galessalazar <gale.s.salazar22@gmail.com>
2025-02-11 23:49:45 -0600

    applied stash

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
   root: "./",
   // name of repo
   base: "/gale_salazar",
   build: {
     outDir: "dist",
     emptyOutDir: true,
-    assetsDir: 'assets',
+    // assetsDir: 'assets',
     rollupOptions: {
       input: './index.html',
     }
