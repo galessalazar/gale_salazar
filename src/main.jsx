b6fa6cf7357da26ff1c0667ad53c7dbc162f9223
galessalazar <gale.s.salazar22@gmail.com>
2025-04-14 10:48:23 -0500

    wrapped app with themeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { ThemeProvider } from "@material-tailwind/react";
+
 import "./App.css";
 import App from "./App.jsx";
-// import './output.css'
+import "./output.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router basename="/">
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Router>
   </StrictMode>
 );
