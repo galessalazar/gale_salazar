1db3782a8b8c49eae3505e114b85502ae84c664a
galessalazar <gale.s.salazar22@gmail.com>
2025-04-14 10:41:20 -0500

    installed material-tailwinds

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import App from "./App.jsx";
-import './output.css'
+// import './output.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+// since using ESM need to use import...from....and export default instead of copy/pasting from docs
+// https://www.material-tailwind.com/docs/react/guide/vite
+
+import withMt from "@material-tailwind/react/utils/withMt"
+
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+export default withMt({
+  content: ['./index.html','./src/**/*.{vue,js,jsx,ts,tsx}',
+   "./client/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./client/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+})
 
