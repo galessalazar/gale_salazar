4cd01e99742f885dde291a29ac241716f0bde93d
galessalazar <gale.s.salazar22@gmail.com>
2025-04-17 12:21:21 -0500

    fixed withMT caps issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 // since using ESM need to use import...from....and export default instead of copy/pasting from docs
 // https://www.material-tailwind.com/docs/react/guide/vite
 
-import withMt from "@material-tailwind/react/utils/withMt"
+import withMT from "@material-tailwind/react/utils/withMT"
 
 
 /** @type {import('tailwindcss').Config} */
-export default withMt({
+export default withMT({
   content: ['./index.html','./src/**/*.{vue,js,jsx,ts,tsx}',
    "./client/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "./client/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
